12b83e37821ea65430d1b3a2d72a8440a4f92a97
gaofer <260418437@qq.com>
2019-12-20 11:13:53 +0800

    完成了一些基本内容

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Pindex from '../views/pindex.vue'
+import Login from  '../views/login.vue'
+import Reg from '../views/reg.vue'
+import Post from '../views/post.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +12,65 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      title: '登录页',
+      keepAlive: true, // 需要被缓存
+    }
   },
+
   {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: '关于',
+      keepAlive: true, // 需要被缓存
+    }
+  },
+  {
+    path: '/pindex',
+    name: 'pindex',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Pindex,
+    meta: {
+      title: '主页',
+      keepAlive: true, // 需要被缓存
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: {
+      title: '登录',
+      keepAlive: true, // 需要被缓存
+    }
+  },
+  {
+    path: '/reg',
+    name: 'reg',
+    component: Reg,
+    meta: {
+      title: '注册',
+      keepAlive: true, // 需要被缓存
+    }
+  },
+  {
+    path: '/post',
+    name: 'post',
+    component: Post,
+    meta: {
+      title: '发帖',
+      keepAlive: true, // 需要被缓存
+    }
   }
+
 ]
 
 const router = new VueRouter({
@@ -27,3 +80,23 @@ const router = new VueRouter({
 })
 
 export default router
+
+router.beforeEach((to, from, next) => {
+  window.document.title = to.meta.title == undefined?'默认标题':to.meta.title
+  if (to.meta.requireAuth) {
+    let token = Cookies.get('access_token');
+    let anonymous = Cookies.get('user_name');
+    if (token) {
+
+      next({
+        path: '/login',
+        query: {
+          redirect: to.fullPath
+        }
+      })
+
+    }
+  }
+  next();
+})
+
